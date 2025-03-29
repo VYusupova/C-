62dea608c84c66df0af5b891e256ebedf061a268
veronika <vopengame@gmail.com>
2025-03-29 11:30:00 +0300

    v1.02 tetris.h

--- frogger/inc/tetris.h
+++ frogger/inc/tetris.h
@@ -3,10 +3,17 @@
 #include <stdlib.h>
 
 void initFigure(figura *f);
+void iniFigura_Q(figura *f);
+void iniFigura_I(figura *f);
+void iniFigura_S(figura *f);
+void iniFigura_Z(figura *f);
+void iniFigura_L(figura *f);
+void iniFigura_J(figura *f);
+void iniFigura_T(figura *f);
+
 void rotateFigure(figura *f);
 void initGameField(game_stats_t *gameBakend);
 void figuraGamefield(game_stats_t *gb, figura *f);
 
 void stats_init(game_stats_t *gameBakend);
 
-void initialFigura_I(figura *f);
