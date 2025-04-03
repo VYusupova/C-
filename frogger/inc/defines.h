3664606d81089055454f82df24da301325759a2f
veronika <vopengame@gmail.com>
2025-04-03 16:29:54 +0300

    v1.06

--- frogger/inc/defines.h
+++ frogger/inc/defines.h
@@ -11,18 +11,19 @@
 #define FSIZE  4  // размер матрицы для фигуры
 #define GET_USER_INPUT getch()
 
-#define INTRO_MESSAGE_LEN     10
-#define INTRO_MESSAGE "Press ENTER" // приветствие
-#define HIDE_INTRO    "           "
+#define INTRO_MESSAGE_LEN     5
+#define INTRO_MESSAGE1 "Press" // приветствие
+#define INTRO_MESSAGE2 "ENTER" // приветствие
+#define HIDE_INTRO    "     "
 
-#define BOARDS_BEGIN 0            // начало игрового поля левый верхний угол с отступом на 2 позиции
+#define BOARDS_BEGIN 0            // начало игрового поля левый верхний угол с отступом на 0 позиции
 #define START_X      (BOARD_M / 2)  // начальные позиции фигуры для игры
 #define START_Y      (0)
 #define BOARD_N     20// (ROWS_MAP + MAP_PADDING * 2) // ширина и высота игрового поля
 #define BOARD_M     10
 #define HUD_WIDTH   10  // ширана  поля для вывода информации по уровням и т.д.
 
-#define PRINT(x, y) mvprintw(BOARDS_BEGIN + (y), BOARDS_BEGIN + (x), " ")
+#define PRINT(x, y) mvprintw(BOARDS_BEGIN + (y), BOARDS_BEGIN + (x), " ") // функция рисования с задаными координатами
 #define MVPRINTW(y, x, ...) mvprintw(BOARDS_BEGIN + (y), BOARDS_BEGIN + (x), __VA_ARGS__)
 #define MVADDCH(y, x, c) mvaddch(BOARDS_BEGIN + (y), BOARDS_BEGIN + (x), c)
 #define CLEAR_BACKPOS(y, x) { bkgdset(COLOR_PAIR(0)); mvaddch(BOARDS_BEGIN + (y), BOARDS_BEGIN + (x), ' ');}
@@ -51,7 +52,8 @@
 #define ENTER_KEY 10
 
 #define SHIFT_MESSAGE 4
-#define R_NEXT 1
+#define R_NEXT 1   // координаты отрисовки поля под новую фигуру
+#define R_NEXT_H 7 
 #define R_LEV 11
 #define R_SCORE 13
 #define R_SPEED 15
