f0f403cf6956ac88d98f60f809886fb1bbae321e
veronika <vopengame@gmail.com>
2025-04-14 16:21:06 +0300

    v 1.08

--- frogger/src/frogger.c
+++ frogger/src/frogger.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include "frogger.h"
+
+#include <stdio.h>
 #include <time.h>
 
 int main(void) {
-    
   WIN_INIT(250);
   setlocale(LC_ALL, "");
-  print_overlay(); // отрисовка поля игры
+  print_overlay();  // отрисовка поля игры
   game_loop();
   endwin();
 
@@ -15,32 +15,30 @@ int main(void) {
 
 void game_loop() {
   board_t map;
-  game_stats_t gameBakend; // здесь лежит массив с описанием игрового поля
+  game_stats_t gameBakend;  // здесь лежит массив с описанием игрового поля
   player_pos frog;
   figura fnow;
   figura fnext;
 
-
   bool break_flag = TRUE;
   int signal = 0;
   frog_state state = START;
-
-  stats_init(&gameBakend);  // задаем начальные значения левел и количество очков
+  // задаем начальные значения левел и количество очков
   gameBakend.fnow = &fnow;
   gameBakend.fnext = &fnext;
-  initColors();       // инициализация цветов
+  initGame(&gameBakend);
 
+  initColors();  // инициализация цветов
 
   while (break_flag) {  // GAMEOVER  - не должен заканчивать цикл???
     if (state == GAMEOVER || state == EXIT_STATE || state == FILE_ERROR_STATE)
       break_flag = FALSE;
-    
-   
+
     printGameField(&gameBakend);
     sigact(get_signal(signal), &state, &gameBakend, &map, &frog, &fnow);
-      timeout (1200);
+    timeout(1200);
     // delay_output(200);
-     movedown(&state, &fnow, &gameBakend);
+    movedown(&state, &fnow, &gameBakend);
 
     if (state == MOVING || state == START) signal = GET_USER_INPUT;
   }
