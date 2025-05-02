49d77a7fa5749a7ea7b4aa513f0f9bc371dceacf
veronika <vopengame@gmail.com>
2025-05-02 16:53:23 +0300

    v1.15

--- s21_tetris/inc/fsm.h
+++ s21_tetris/inc/fsm.h
@@ -2,24 +2,35 @@
 #define FSM_H
 
 #include "defines.h"
-#include "objects.h"
-
 #include "frog_frontend.h"
+#include "objects.h"
 #include "tetris.h"
 
+typedef enum { GAME = 0, 
+               MOVING, 
+               GAMEOVER 
+} tetris_state;
+
+void sigact(UserAction_t *userAct, tetris_state *state, game_stats_t *gamestats,
+            figura *fnow);
+void started(UserAction_t *userAct, game_stats_t *game, tetris_state *state);
+void moved(UserAction_t *userAct, tetris_state *state, game_stats_t *gamestats,
+            figura *fnow);
+            void gg(game_stats_t *game, tetris_state *state);
 
 
+void movedown(/*UserAction_t *userAct,*/ tetris_state *state,
+              figura *moveFigure, game_stats_t *gamestats);
 
 UserAction_t get_signal(int user_input);
-void sigact(UserAction_t *state, game_stats_t *stats, figura *posStart); //board_t *map,
-			 
-//void on_start_state(signals sig, frog_state *state, game_stats_t *game);
-void movedown(UserAction_t *state,  figura *moveFigure,  game_stats_t *gamestats);
-void moveleft(figura *moveFigure,  game_stats_t *gamestats);
-int check(figura *f,  game_stats_t *gb);
+
+// void on_start_state(signals sig, frog_state *state, game_stats_t *game);
+
+void moveleft(figura *moveFigure, game_stats_t *gamestats);
+int check(figura *f, game_stats_t *gb);
 
 int collisionDown(figura *f, game_stats_t *gb);
 
-void start(game_stats_t *game);
+
 
 #endif
