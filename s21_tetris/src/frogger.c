c62d9c4f7eb7546c85b39d4ec5415449f11ff024
veronika <vopengame@gmail.com>
2025-05-02 16:50:02 +0300

    v1.15

--- s21_tetris/src/frogger.c
+++ s21_tetris/src/frogger.c
@@ -1,13 +1,14 @@
-#include "frogger.h"
 
 #include <stdio.h>
-#include <time.h>
 #include <tetris.h>
+#include <time.h>
+
+#include "fsm.h"
 
 int main(void) {
   WIN_INIT(250);
   setlocale(LC_ALL, "");
-  initColors();  // инициализация цветов  
+  initColors();     // инициализация цветов
   print_overlay();  // отрисовка поля игры
   game_loop();
   endwin();
@@ -17,32 +18,35 @@ int main(void) {
 
 void game_loop() {
   game_stats_t gameBakend;  // здесь лежит массив с описанием игрового поля
- 
+
   figura fnow;
   figura fnext;
 
   bool break_flag = TRUE;
 
   UserAction_t userAct = Pause;
+  tetris_state state = GAME;
   // задаем начальные значения левел и количество очков
   gameBakend.fnow = &fnow;
   gameBakend.fnext = &fnext;
   initGame(&gameBakend);
 
-
-
-  while (break_flag) {  
-    if (userAct == Terminate) 
+  while (break_flag) {
+    if (state == GAMEOVER) {
       break_flag = FALSE;
+      // gameOver();
+    }
 
-    //printGameField(&gameBakend); //?
-    sigact(&userAct, &gameBakend, &fnow);  
-       //if (state != Terminate) signal = GET_USER_INPUT; //state == MOVING || state == Start
-    userAct = get_signal(GET_USER_INPUT);
-    //movedown(&userAct, &fnow, &gameBakend);
-    //napms(500);
+   printGameField(&gameBakend);
+   
+    sigact(&userAct, &state, &gameBakend, &fnow);
 
-    timeout(700); 
+    if (state == MOVING || state == GAME) userAct = get_signal(GET_USER_INPUT);
+    // else userAct = Terminate;
+    timeout(700);
+    // movedown(&userAct, &fnow, &gameBakend);
+    // napms(500);
+
     // delay_output(200);
   }
   writeScore(&gameBakend);
