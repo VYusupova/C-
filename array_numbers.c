9725c6e1d6f6ed0ea17b1a087619d1b1eb67f1ec
Kate <lymond@ex.com>
2025-07-24 11:45:25 +0300

    exam trying

--- array_numbers.c
+++ array_numbers.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-#define N 100
-
-int * read_arr(int * len) {
+static int * read_arr(int * len) {
     int capacity = 10;
     int * arr = (int * ) calloc(capacity, sizeof(int));
     if (NULL == arr) return NULL;
@@ -14,12 +11,12 @@ int * read_arr(int * len) {
         else {
             arr[i] = a;
             i++;
-            if (capacity < i) {
+            // chek size for next element (<=)
+            if (capacity <= i) {
                 capacity += 5;
-                int *new_arr = (int*)realloc((void*)arr, (size_t)(15*sizeof(int)));
-                if (NULL == new_arr) return NULL;
-                arr = new_arr;
-                printf("realloc %d ", capacity);
+                unsigned size = capacity*sizeof(int);
+                arr = (int*)realloc((void*)arr, size);
+                if (NULL == arr) return NULL;
             }
         }
     }
@@ -27,7 +24,7 @@ int * read_arr(int * len) {
     return arr;
 }
 
-void print_arr(int * arr, int len) {
+static void print_arr(const int * arr, int len) {
     for (int i = 0; i < len; i++) {
         printf("%d ", arr[i]);
     }
@@ -39,7 +36,7 @@ int main() {
     int len = 0;
     int * arr = read_arr( & len);
     if (arr) print_arr(arr, len);
-    //if (arr != NULL) free(arr);
+    if (arr != NULL) free(arr);
 
     printf("GOOD");
 
--- str_no_space.c
+++ str_no_space.c
@@ -1,48 +1,54 @@
 /*Задача на работу со строками 
    на вход подается строка ее надо отформатировать так что бы
    не было пробелов вначале и конце строки
-   между словами по одному пробелму  */
+   между словами по одному пробелу  */
    
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_str(char * str) {
+static void print_str(const char * str) {
     int i = 0;
     char space = ' ';
     // посчитаем длину строки
     while (str[i] != '\0') {
+        if (str[i] == '\n') break;
         i++;
     }
     //  выделим память под новую строку
-    char * out = malloc(i);
+
+    char * out = NULL;
+    if (i > 0) out = malloc(i*sizeof(char));
+    if (out == NULL) return;
     i = 0;
     int j = 0;
     // пропустим начальные пробелы
     while (str[i] == space) {
         i++;
     }
-    while (str[i] != '\0') {
+    while (str[i] != '\0' )  {
         if (str[i] == '\n') break;
-        if (space == str[i] && str[i - 1] == space) {}
+        if (space == str[i] && str[i - 1] == space) {i++;}
         else {
             out[j] = str[i];
-            j++;
+            j++; 
         }
-        i++;
     }
-    if (out[j - 1] == space) out[j - 1] = '\0';
-    printf("%s", out);
-    if (out) free(out);
+    if (out[j - 1] == space) out[j - 1] = '\0';    
+    if (out != NULL) {printf("%s", out);free(out);}
 }
 
 int main() {
     char * str = NULL;
     size_t len = 0;
-    ssize_t read = getline( & str, & len, stdin);
-    if (read != -1) {
-        //printf("%s",str);
-        printf("<");
-        print_str(str);
-        printf(">");
+    size_t read = getline( & str, & len, stdin);
+    if (read != (unsigned long int)-1) {
+        if (str != NULL) {
+        	//printf("%s",str);
+        	printf("<");
+       	print_str(str);
+        	printf(">");
+        	free(str);
+        }
     }
     else {
         printf("Error");
