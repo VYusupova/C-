668d4cabd16592fdee07d45944efcef68349c102
veronika <vopengame@gmail.com>
2025-08-27 11:47:51 +0300

    Update s21_graph.c

--- s21_graph.c
+++ s21_graph.c
@@ -3,7 +3,52 @@
 
 #include "s21_graph.h"
 
-#include "../lib/s21_matrix.h"
+
+void print(s21_graph g) {
+    printf("size graph %02d\n ", g.row);
+    printf("element graph\n");
+    for (int i = 0; i < g.row; i++) {
+        for (int j = 0; j < g.row; j++)
+            printf(" %2d", g.matrix[i][j]);
+        printf("\n");
+    }
+}
+
+int ** create_matrix(int row) {
+    int ** result;
+    if (row < 1) {
+        perror(_ERR_SIZE);
+        return NULL;
+    }
+
+    result = (int ** ) calloc(row, sizeof(int * ));
+
+    if (!result) {
+        perror(_ERR_CALLOC);
+        return NULL;
+    }
+    for (int i = 0; i < row; i++) {
+        result[i] = (int * ) calloc(row, sizeof(int));
+
+        if (!result[i]) {
+            while (i--) free(result[i]);
+            free(result);
+            perror(_ERR_CALLOC);
+            return NULL;
+        }
+    }
+
+    return result;
+}
+
+void remove_graph(s21_graph *g) {
+  if (g->matrix != NULL) {
+    for (int i = 0; i < g->row; i++) free(g->matrix[i]);
+    free(g->matrix);
+    g->matrix = NULL;
+    g->row = 0;
+  }
+}
 
 static void read_graph_el(FILE *f, matrix_t *graph) {
   for (int i = 0; i < graph->rows; i++)
