02e86dee80318932588510735b7586a598a0fc1b
veronika <vopengame@gmail.com>
2025-09-04 10:11:40 +0300

    export_graph.c

--- s21_graph.c
+++ s21_graph.c
@@ -3,39 +3,7 @@
 
 #include "s21_graph.h"
 
-static void print_graph(s21_graph *graph) {
-  /*char temp[256];
-  strncpy(temp, filename, sizeof(temp) - 1);
-  temp[sizeof(temp) - 1] = '\0';
 
-  // Ищем расширение .txt
-  char *ext = strstr(temp, ".txt");
-  if (ext != NULL &&
-      strlen(ext) == 4) { // Проверяем что это именно конец строки
-    *ext = '\0'; // Обрезаем
-  }
-
-  int digraph = 0; // is_digraph(graph);
-  if (digraph == 0) {
-    printf("graph %s {\n", temp);
-  } else {
-    printf("digraph %s {\n", temp);
-  }
-  for (int i = 0; i < graph->size; i++) {
-    printf("%c;\n", 'A' + i);
-  }
-  for (int i = 0; i < graph->size; i++) {
-    for (int j = i; j < graph->size; j++) {
-      if (i != j && graph->matrix[i][j] != 0 && digraph == 0) {
-        printf("%c -- %c;\n", 'A' + i, 'A' + j);
-      } else if (graph->matrix[i][j] != 0 && graph->matrix[i][j] != -1 &&
-                 digraph == 1) {
-        printf("%c -> %c;\n", 'A' + i, 'A' + j);
-      }
-    }
-  }
-  printf("}\n");*/
-}
 
 static void print(s21_graph *g) {
   printf("size graph %02d\n", g->size);
@@ -126,36 +94,31 @@ static void load_graph(char *filename, s21_graph *graph) {
   printf("load graph end \n");
 }
 
-static int is_directed(const s21_graph *g) {
-  for (int i = 0; i < g->size; i++) {
-    for (int j = 1; j < i; j++) {
-      if (g->matrix[i][j] != g->matrix[j][i]) {
-        return FALSE;
-      }
-    }
-  }
-  return TRUE;
+static int is_digraph(const s21_graph *graph) {
+	int result = 0;
+	for(int i = 0; i < graph->size; i++) {
+		for(int j = 0; j < graph->size; j++) {
+			if(graph->matrix[i][j] != graph->matrix[j][i]) {
+				result = 1;
+			}
+		}
+	}
+	return result;
 }
 
+
 int export_graph(char *filename, s21_graph *g) {
+  if (g->size == 0) return -1;
   FILE *file = fopen(filename, "w");
-  if (!file)
-    return -1;
+  if (!file) return -1;
 
-  int isdirected = is_directed(g);
+  int digraph = is_digraph(graph);
 
-  if (!isdirected) {
+  if (!digraph) {
     fprintf(file, "graph G {\n");
-    for (int i = 0; i < g->size; i++) {
-      for (int j = i; j < g->size; j++) {
-        if (i != j && g->matrix[i][j] != 0) {
-          fprintf(file, "\t%d -- %d [weight=%d];\n", i + 1, j + 1,
-                  g->matrix[i][j]);
-        }
-      }
-    }
   } else {
     fprintf(file, "digraph G {\n");
+  }
     for (int i = 0; i < g->size; i++) {
       for (int j = 0; j < g->size; j++) {
         if (g->matrix[i][j] != 0) {
@@ -164,11 +127,34 @@ int export_graph(char *filename, s21_graph *g) {
         }
       }
     }
-  }
 
   fprintf(file, "}\n");
   fclose(file);
   return 0;
+}
+
+static void print_graph(s21_graph *graph) {
+  if (g->size == 0) perroe(_ERR_SIZE);
+  int digraph =  is_digraph(graph);
+  if (digraph == 0) {
+    printf("graph {\n", );
+  } else {
+    printf("digraph {\n");
+  }
+  for (int i = 0; i < graph->size; i++) {
+    printf("%c;\n", 'A' + i);
+  }
+  for (int i = 0; i < graph->size; i++) {
+    for (int j = i; j < graph->size; j++) {
+      if (i != j && graph->matrix[i][j] != 0 && digraph == 0) {
+        printf("%c -- %c;\n", 'A' + i, 'A' + j);
+      } else if (graph->matrix[i][j] != 0 && graph->matrix[i][j] != -1 &&
+                 digraph == 1) {
+        printf("%c -> %c;\n", 'A' + i, 'A' + j);
+      }
+    }
+  }
+  printf("}\n");
 }
 
 s21_graph graph_init() {
