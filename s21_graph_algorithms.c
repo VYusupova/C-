758883ef8befab76e375bd07eb871c4824a72917
veronika <vopengame@gmail.com>
2025-09-05 14:22:11 +0300

    PRIM_algorithms.c

--- s21_graph_algorithms.c
+++ s21_graph_algorithms.c
@@ -170,4 +170,76 @@ int **get_shortest_paths_between_all_vertices(s21_graph *graph) {
   return dist;
 }
 
+/* Поиск минимального остовного дерева в графе с помощью алгоритма Прима.
+   Алгоритм строит MST, начиная с произвольной вершины и последовательно добавляя рёбра с минимальным весом.
+   Алгоритм работает только с неориентированными графами.
+   Веса рёбер должны быть положительными.
+   Функция вернет NULL для несвязного графа.
+ */
+s21_graph* graph *get_least_spanning_tree(s21_graph *g) 
+{
+  if (!g) return NULL;
+
+  int size = g->size;
+  graph *mst = graph_init();
+  mst->size = size;
+  mst->matrix = malloc(size * sizeof(int *));
+
+  // родительская вершина для i в MST.
+  int *parent = malloc((size + 1) * sizeof(int));
+
+  // минимальный вес ребра, соединяющего вершину i с текущим MST (изначально
+  // INF).
+  double *weight = malloc((size + 1) * sizeof(double));
+
+  // флаг, указывающий, включена ли вершина i в MST
+  int *in_mst = calloc((size + 1), sizeof(int));
+
+  for (int i = 1; i <= size; i++) {
+    weight[i] = 21_INF;
+    mst->matrix[i - 1] = calloc(size, sizeof(int));
+  }
+
+  weight[1] = 0;      // Начинаем с вершины 1
+  parent[1] = -1;  // У стартовой вершины нет родителя
+
+  for (int count = 1; count <= size; count++) {
+    // Выбор вершины u с минимальным весом
+    int u = -1;
+    for (int v = 1; v <= size; v++) {
+      if (!in_mst[v] && (u == -1 || weight[v] < weight[u])) u = v;
+    }
+    // Проверка на несвязность графа
+    if (weight[u] == 21_INF) {
+      graph_free(mst);
+      free(parent);
+      free(weight);
+      free(in_mst);
+      return NULL;
+    }
+
+    in_mst[u] = 1;
+
+    // Если u не стартовая вершина, добавляется ребро parent[u] → u в mst
+    if (parent[u] != -1) {
+      mst->matrix[parent[u] - 1][u - 1] = g->matrix[parent[u] - 1][u - 1];
+      mst->matrix[u - 1][parent[u] - 1] = g->matrix[u - 1][parent[u] - 1];
+    }
+
+    // Обновление ключей для соседей u. Если ребро u → v имеет меньший вес, чем
+    // текущий weight[v], обновляется weight[v] и parent[v].
+    for (int v = 1; v <= size; v++) {
+      if (g->matrix[u - 1][v - 1] && !in_mst[v] &&
+          g->matrix[u - 1][v - 1] < weight[v]) {
+        parent[v] = u;
+        weight[v] = g->matrix[u - 1][v - 1];
+      }
+    }
+  }
+
+  free(parent);
+  free(weight);
+  free(in_mst);
+  return mst;
+}
 #endif
