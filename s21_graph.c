b6a444648fd38a389a53e79f16af8d07687c78d9
veronika <vopengame@gmail.com>
2025-09-08 10:41:47 +0300

    Update s21_graph.c

--- s21_graph.c
+++ s21_graph.c
@@ -3,6 +3,56 @@
 
 #include "s21_graph.h"
 
+graph *graph_create() {
+	graph *graph = malloc(sizeof(graph));
+	if(graph) {
+		graph->matrix = NULL;
+		graph->size = 0;
+    graph.load_graph_from_file = &load_graph;
+    graph.export_graph_to_dot = &export_graph;
+    graph.del_graph = &remove_graph;
+    graph.is_digraph = &is_digraph;
+	}
+	return graph;
+}
+
+/*
+static void graph_free(graph *graph) {
+	if(graph) {
+		if(graph->matrix) {
+			for(int i = 0; i < graph->size; i++) {
+				free(graph->matrix[i]);
+			}
+			free(graph->matrix);
+		}
+	free(graph);
+	}
+}
+*/
+
+graph *load_graph_from_file(char *filename) {
+	FILE *file = fopen(filename, "r");
+	if(!file) return NULL;
+
+	graph *graph = graph_create();
+  load_graph(filename, graph);
+
+	
+	return graph;
+}
+
+static int is_digraph(const s21_graph *graph) {
+  int result = 0;
+  for (int i = 0; i < graph->size; i++) {
+    for (int j = 0; j < graph->size; j++) {
+      if (graph->matrix[i][j] != graph->matrix[j][i]) {
+        result = 1;
+      }
+    }
+  }
+  return result;
+}
+
 static int **calloc_matrix(int size) {
   int **result;
   if (size < 1)
@@ -78,17 +128,7 @@ static void load_graph(char *filename, s21_graph *graph) {
   printf("load graph end \n");
 }
 
-static int is_digraph(const s21_graph *graph) {
-  int result = 0;
-  for (int i = 0; i < graph->size; i++) {
-    for (int j = 0; j < graph->size; j++) {
-      if (graph->matrix[i][j] != graph->matrix[j][i]) {
-        result = 1;
-      }
-    }
-  }
-  return result;
-}
+
 
 int export_graph(char *filename, s21_graph *g) {
   if (g->size == 0) return -1;
@@ -121,16 +161,16 @@ int export_graph(char *filename, s21_graph *g) {
   fclose(file);
   return 0;
 }
-
+/*
 s21_graph graph_init() {
-  s21_graph g;  //= malloc(sizeof(s21_graph));
+  s21_graph g = graph_create();
   g.load_graph_from_file = &load_graph;
   g.export_graph_to_dot = &export_graph;
   g.del_graph = &remove_graph;
   g.is_digraph = &is_digraph;
-  g.size = 0;
-  g.matrix = NULL;
+  //g.size = 0;
+  //g.matrix = NULL;
   return g;
 }
-
+*/
 #endif
