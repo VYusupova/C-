68ad4fdd6b413001d360b2388029b92648d61876
veronika <vopengame@gmail.com>
2025-09-08 13:51:14 +0300

    Update greed.c

--- graph/greed.c
+++ graph/greed.c
@@ -1,55 +1,60 @@
-tsm_result *greedy_tsp(graph *g) {
-  if (!g || g->size < 2) return NULL;
-  tsm_result *res = calloc_tsm(res, g->size); 
-  int *visited = calloc(g->size, sizeof(int));
-  int current = 0;
-  res->vertices[0] = 1;  // Стартовая вершина: Фиксирована (вершина 1 в коде).
-  visited[0] = 1;
-  for (int i = 1; i < g->size; i++) {
-    int next = -1;
-    int min_dist = INT_MAX;
-    for (int j = 0; j < g->size; j++) {
-      int edge =  g->matrix[current][j];
-      if (!visited[j] && edge > 0 && edge < min_dist) {
-        min_dist = edge;
-        next = j;
-      }
-    }
-    if (next == -1) {
-      free(res);
-      break;
-    }
-    res->vertices[i] = next + 1;
-    res->distance += min_dist;
-    visited[next] = 1;
-    current = next;
-  }
-  if (res) {
-    // проверяем существует ли обратное ребро
-    if (g->matrix[current][0] <= 0) {
+static tsm_result * calloc_tsm(tsm_result * t, int size) {
+    tsm_result * tsm = malloc(sizeof(tsm_result));
+    tsm -> vertices = malloc((size + 1) * sizeof(int));
+    tsm -> distance = 0;
+    return tsm;
+}
+
+static void free_tsm(tsm_result * t) {
+    free(res -> vertices);
     free(res);
-    } else {
-      res->vertices[g->size] = 1;
-      res->distance += g->matrix[current][0];
+    res = NULL;
+}
+
+static int next_min_node(int * visited, graph * g, int *min_dist ) {
+    int next = -1;
+    for (int j = 0; j < g -> size; j++) {
+        int edge = g -> matrix[current][j];
+        if (!visited[j] && edge > 0 && edge < min_dist) {
+            min_dist = edge;
+            next = j;
+        }
     }
-  }
-  free(visited);
-  return res;
+    return next;
 }
 
-int not_visit_and_min(int node){
- return
-   }
-
-static void free_tsm(tsm_result *t){
-      free(res->vertices);
-      free(res);
-      res = NULL;
-}
-
-static tsm_result * calloc_tsm(tsm_result *t, int size){
-  tsm_result *tsm = malloc(sizeof(tsm_result));
-  tsm->vertices = malloc((size + 1) * sizeof(int));
-  tsm->distance = 0;
-  return tsm;
+tsm_result * greedy_tsp(graph * g) {
+    if (g -> size == 0)
+        perror("_ERR_GRAPH_IS_EMPTY");
+    else {
+        tsm_result * res = calloc_tsm(res, g -> size);
+        int * visited = calloc(g -> size, sizeof(int));
+        int current = 0;
+        res -> vertices[0] = 1;
+        visited[0] = 1;
+        for (int i = 1; i < g -> size; i++) {
+            int next = -1;
+            int min_dist = INT_MAX;
+            next = next_min_node(visited, g, min_dist ) ;
+            if (next == -1) {
+                free_tsm(res);
+                break;
+            }
+            res -> vertices[i] = next + 1;
+            res -> distance += min_dist;
+            visited[next] = 1;
+            current = next;
+        }
+        if (res) {
+            // проверяем существует ли обратное ребро
+            if (g -> matrix[current][0] <= 0) {
+                free(res);
+            } else {
+                res -> vertices[g -> size] = 1;
+                res -> distance += g -> matrix[current][0];
+            }
+        }
+        free(visited);
+    }
+    return res;
 }
