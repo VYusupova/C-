5a414c10797305dd252385e1cf77d9a4ad038618
veronika <vopengame@gmail.com>
2025-09-15 12:38:06 +0300

    Update str_no_space_3.c

--- str_no_space_3.c
+++ str_no_space_3.c
@@ -1,22 +1,29 @@
-/*Задача на работу со строками 
+/*Задача на работу со строками
    на вход подается строка ее надо отформатировать так что бы
    не было пробелов вначале и конце строки
    между словами по одному пробелу  */
-   /* 3 вариант с ипользованием конструкции     scanf("%ms", &str);*/
+/* 3й вариант с ипользованием конструкции     scanf("%ms", &str);
+не работающий не доработанный цикл бесконечный*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    char * str = NULL;
-        printf("<");
-    scanf("%ms", &str);
-    if (str != NULL) printf("%s", str);
-    while(1){
-        if (str == NULL) break;
-        printf(" %s", str);
-        free(str);
-        scanf("%ms", &str);
+  char* str = NULL;
+  printf("<");
+  scanf("%ms", &str);
+  if (str != NULL) printf("%s", str);
+  while (1) {
+    if (str == NULL) break;
+    printf(" %s", str);
+    free(str);
+    if (0 == scanf("%ms", &str)) break;
+    int i = 0;
+    while (str[i] != '\0') {
+      if (str[i] == '\n') printf("str[i]");
+      i++;
     }
-        free(str);
-        printf(">");
+    if (str[0] == '\0') break;
+  }
+  free(str);
+  printf(">");
 }
