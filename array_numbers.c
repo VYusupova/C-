a47f0737f110d761c21e549f10b0b43185737532
veronika <vopengame@gmail.com>
2025-09-15 10:39:52 +0300

    Update array_numbers.c

--- array_numbers.c
+++ array_numbers.c
@@ -1,43 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int * read_arr(int * len) {
-    int capacity = 10;
-    int * arr = (int * ) calloc(capacity, sizeof(int));
-    if (NULL == arr) return NULL;
-    int a, i = 0;
-    while (1 == scanf("%d", & a)) {
-        if (a == -1) break;
-        else {
-            arr[i] = a;
-            i++;
-            // chek size for next element (<=)
-            if (capacity <= i) {
-                capacity += 5;
-                unsigned size = capacity*sizeof(int);
-                arr = (int*)realloc((void*)arr, size);
-                if (NULL == arr) return NULL;
-            }
-        }
-    }
-    * len = i;
-    return arr;
+static void my_realloc(int new_size, int* a) {
+  unsigned size = new_size * sizeof(int);
+  int* temp = (int*)realloc((void*)a, size);
+  if (temp)
+    a = temp;
+  else {
+    free(a);
+    a = NULL;
+  }
 }
 
-static void print_arr(const int * arr, int len) {
-    for (int i = 0; i < len; i++) {
-        printf("%d ", arr[i]);
+/*функция считывания массива целых чисел */
+
+static int* read_arr(int* len) {
+  int capacity = 0;
+  int* arr = (int*)calloc(capacity, sizeof(int));
+  if (NULL == arr) return NULL;
+  int a, i = 0;
+  while (1 != scanf("%d", &a)) {
+    while (getchar() != ' ');
+  }
+  while (a != -1) {
+    /*Признак конца строки -1*/
+    arr[i] = a;
+    i++;
+    if (capacity <= i) {
+      my_realloc(capacity + 5, arr);
+      if (NULL == arr) return NULL;
     }
+    /*Проверка на тот случай если в строке ввода будут символы а не числа
+    в цикле очищаем буфер ввода пока не встретим конец строки или пробел*/
+    while (1 != scanf("%d", &a)) {
+      while (getchar() != ' ');
+    }
+  }
+  *len = i;
+  return arr;
 }
+
+static void print_arr(const int* arr, int len) {
+  for (int i = 0; i < len; i++) {
+    printf("%d ", arr[i]);
+  }
+}
+/* стоки для проверки
+1 2 3 4 -1
+asd 1 2 3 -1
+1 2 3 4 dfsd sdfsdfw fhhgfth rggre 5 8 9 -1
+1 2 5 d d d3 -1
+1 2 3 4 d g gddf 1d 2d3 -1
+*/
 
 int main() {
-
-
-    int len = 0;
-    int * arr = read_arr( & len);
-    if (arr) print_arr(arr, len);
-    if (arr != NULL) free(arr);
-
-    printf("GOOD");
-
+  int len = 5;
+  int* arr = read_arr(&len);
+  if (arr) {
+    print_arr(arr, len);
+    free(arr);
+  } else
+    printf("NOT READ");
+  printf("GOOD");
 }
