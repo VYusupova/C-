5fd440633f0faa8d179606279041832db665fcaf
veronika <vopengame@gmail.com>
2025-09-17 10:52:06 +0300

    Update test.h

--- test/test.h
+++ test/test.h
@@ -1,16 +1,15 @@
-﻿#ifndef SRC_TESTS_TEST_H_
+#ifndef SRC_TESTS_TEST_H_
 #define SRC_TESTS_TEST_H_
 
 #include <check.h>
 #include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "../s21_graph.h"
-//#include "../graph_algorithms/s21_graph_algorithms.h"
+#include "../s21_graph_algorithms.h"
 
 Suite *test_graph(void);
 
-// Suite *test_algorithms(void);
+Suite *test_algorithms(void);
 
-#endif // SRC_TESTS_TEST_H_
+#endif
