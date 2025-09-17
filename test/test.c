528c665e7f2c7ec27c882cc1f7e6539d98c36f98
veronika <vopengame@gmail.com>
2025-09-17 10:51:50 +0300

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,12 +1,15 @@
+#ifndef TEST_C
+#define TEST_C
+
 #include "test.h"
 
 int main(void) {
   int number_failed = 0;
-  Suite *test[] = {test_graph(), /* test_algorithms(), */ NULL};
-  SRunner *sr;
+  Suite *test[] = {test_graph(), test_algorithms(), NULL};
+
   int i = 0;
   while (test[i] != NULL) {
-    sr = srunner_create(test[i]);
+    SRunner *sr = srunner_create(test[i]);
 
     srunner_set_fork_status(sr, CK_NOFORK);
     srunner_run_all(sr, CK_NORMAL);
@@ -15,11 +18,11 @@ int main(void) {
     srunner_free(sr);
     i++;
     (number_failed != 0)
-        ? printf("\033[31;1m=========FAILED: %d =========\033[0;0m\n",
-                 number_failed)
-        : printf("\033[32;1m=========FAILED: %d =========\033[0;0m\n",
-                 number_failed);
+        ? printf("\033[31;1mFAILED: %d \033[0;0m\n", number_failed)
+        : printf("\033[32;1mFAILED: %d \033[0;0m\n", number_failed);
   }
 
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
+#endif
